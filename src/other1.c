d78b49d399f372ff0661101674b0827d327b31af
dadongsir <91357795+dadongsir@users.noreply.github.com>
2023-05-15 16:06:33 +0800

    first commit

---
+++ src/other1.c
@@ -0,0 +1,7 @@
+#include "other1.h"
+#include <stdio.h>
+
+void welcome()
+{
+        printf( "Welcome to the world of Linux\n" );
+}
---
+++ src/other2.c
@@ -0,0 +1,11 @@
+#include "other2.h"
+#include <stdio.h>
+int add( int x, int y ) 
+{
+        return x+y;
+}
+int sub( int x, int y )
+{
+        return x-y;
+}
+
